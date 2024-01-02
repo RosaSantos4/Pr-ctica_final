3013193224e8f6dba02dc0d747aad5e89d24af3c
beaaprr <beatrizpr00@gmail.com>
2024-01-02 22:00:57 +0100

    header responsive wip 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 bars = document.querySelector(".bars");
 bars.onclick = function () {
     navBar = document.querySelector (".nav-bar");
-    navBar.classlist.toggle("active");
+    navBar.classList.toggle("active");
 }
