312669be83de11361902e1a20e184a0bc4e72e4a
beaaprr <beatrizpr00@gmail.com>
2024-01-03 18:00:58 +0100

    hover boton comprar

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ bars.onclick = function () {
     navBar = document.querySelector (".nav-bar");
     navBar.classList.toggle("active");
 }
+
+function hover(element){
+    element.setAttribute('src',"media/boton_hover.svg")
+}
+function unhover(element){
+    element.setAttribute('src',"media/boton.svg")
+}
