737c5f0fdc905f00ca8964f7f30dce56c347c7ec
RosaSantos4 <rsantosromero@sva.edu>
2024-01-05 16:41:39 +0100

    mejoras

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
+// menú
 bars = document.querySelector(".bars");
 bars.onclick = function () {
     navBar = document.querySelector (".nav-bar");
     navBar.classList.toggle("active");
 }
 
+// botón
 function hover(element){
     element.setAttribute('src',"media/boton_hover.svg")
 }
 function unhover(element){
     element.setAttribute('src',"media/boton.svg")
+}
+
+flecha_abajo = document.querySelector(".flecha_abajo");
+flecha_abajo.onclick = function () {
+    navBar = document.querySelector (".menu_product");
+    navBar.classList.toggle("active");
 }
